cf601ae0319fcfd4ab223a8c37f770ec8f823ab8
jiangbaotian <394584502@qq.com>
2021-06-18 11:59:45 +0800

    修改配置

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,7 +27,7 @@ repositories {
     mavenCentral()
 }
 dependencies {
-    detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.17.1")
+    detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.15.0")
     compileOnly(files("lib/wizard-template.jar"))
 }
 
